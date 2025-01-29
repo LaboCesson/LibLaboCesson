7f66ea9984ebc9a45e5aada325d6de104faa3fbd
pascal <14837879+pgravoille@users.noreply.github.com>
2025-01-29 09:56:39 +0100

    Ajout de la librairie gérant un clavier

---
+++ LibClavier.cpp
@@ -0,0 +1,32 @@
+/*!
+ * \file 	LibClavier.cpp
+ * \brief 	Le fichier source de la classe LibClavier
+ * \author  LaboCesson
+ * \version 1.0
+ * \date    Janvier 2025
+ */
+
+#include "LibClavier.h"
+
+
+LibClavier::LibClavier( unsigned char nbTouche, char * p_touche,
+                        unsigned char nbPinOut, unsigned char * p_pinOut,
+                        unsigned char nbPinIn,  unsigned char * p_pinIn,
+                        LibMcp23008 * p_pinExt=0 ) {
+  m_nbTouche = nbTouche;
+  mp_touche  = p_touche;
+  mp_matrice = new LibMatrice(nbPinOut, p_pinOut, nbPinIn,  p_pinIn, p_pinExt);
+}
+
+
+void LibClavier::begin(void) {
+  mp_matrice->begin();
+}
+
+
+char LibClavier::getTouche(void) {
+  unsigned char index = mp_matrice->getIndex();
+  if( (index == 0) || (index > m_nbTouche) ) return 0;
+  return mp_touche[index-1];
+}
+
---
+++ LibClavier.h
@@ -0,0 +1,47 @@
+/*!
+ * \file 	LibClavier.h
+ * \brief 	Le fichier de définition de la classe LibClavier
+ * \author  LaboCesson
+ * \version 1.0
+ * \date    Janvier 2025
+ */
+
+#ifndef _LIB_CLAVIER_H_
+#define _LIB_CLAVIER_H_
+
+#include "arduino.h"
+#include "LibMatrice.h"
+
+
+/// \class LibClavier
+/// \brief Gestion d'un clavier sous forme de matrice
+/// \details La librairie peut gérer 16 touches
+class LibClavier
+{
+  public:
+    LibClavier(
+      unsigned char   nbTouche,  ///< Nombre de touche à gérer
+               char * p_touche,  ///< Liste des codes ASCII des touches correspondant
+		  unsigned char   nbPinOut,  ///< Le nombre de lignes entrantes de la matrice du clavier
+		  unsigned char * p_pinOut,  ///< La liste de pins associés aux lignes entrantes
+		  unsigned char   nbPinIn,   ///< Le nombre de lignes sortantes de la matrice
+		  unsigned char * p_pinIn,   ///< La liste de pins associés aux lignes sortantes
+      LibMcp23008   * p_pinExt=0 ///< Pointeur vers l'objet en charge de la gestion d'un MCP23008
+	  );
+
+    /// \details Permet d'initialiser la gestion du clavier
+    void begin(void);
+
+    /// \return Retourne le code ASCII de la touche enfoncée
+    char getTouche(void);
+	
+  private:
+  unsigned char * m_nbTouche;
+           char * mp_touche;
+  unsigned char * m_nbDigit;
+    LibMatrice  * mp_matrice;
+
+  protected:
+};
+
+#endif

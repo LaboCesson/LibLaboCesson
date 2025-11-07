27d6f48153e892bf3d83771b988a4d182caa8b15
pascal <14837879+pgravoille@users.noreply.github.com>
2025-11-07 10:45:43 +0100

    Gestion plus généraliste des Jumpers

--- LibJumper.cpp
+++ LibJumper.cpp
@@ -2,17 +2,22 @@
  * \file 	LibJumper.cpp
  * \brief 	Le fichier source de la classe LibJumper
  * \author  LaboCesson
- * \version 1.0
- * \date    Janvier 2025
+ * \version 2.0
+ * \date    Octobre 2025
  */
 
 #include "LibJumper.h"
 
 
-LibJumper::LibJumper(unsigned char pinJumperTeam, unsigned char pinJumperNbH, unsigned char pinJumperNbL) {
+LibJumper::LibJumper(unsigned char pinJumperTeam, unsigned char * jumpersPinList, unsigned char   nbPinInTab ) {
   pinMode(m_pinJumperTeam = pinJumperTeam, INPUT_PULLUP);  
-  pinMode(m_pinJumperNbH  = pinJumperNbH,  INPUT);
-  pinMode(m_pinJumperNbL  = pinJumperNbL,  INPUT);
+
+  m_nbJumpers = nbPinInTab;
+  if (m_nbJumpers > MAX_NB_JUMPERS) m_nbJumpers = MAX_NB_JUMPERS; // Par sécurité
+  for (unsigned char i = 0; i < m_nbJumpers; i++) {
+    m_jumpers[i] = jumpersPinList[i];
+    pinMode(m_jumpers[i],  INPUT_PULLUP);
+  }
 }
 
 
@@ -21,10 +26,23 @@ t_pamiTeam LibJumper::getTeam(void) {
 }
 
 
-t_pamiNumber LibJumper::getPami(void) {
-  if( analogRead(m_pinJumperNbH) > 100 ) {
-    return( ( analogRead(m_pinJumperNbL) > 100 ? PAMI_SING : PAMI_NB_1 ) ) ;
-  } else {
-    return( ( analogRead(m_pinJumperNbL) > 100 ? PAMI_NB_2 : PAMI_NB_3) ) ;
-  }
+unsigned char LibJumper::getPami(void) {
+  #ifdef AVR
+ 
+    if( analogRead(m_jumpers[1]) > 100 ) {
+      return( ( analogRead(m_jumpers[0]) > 100 ? 0 : 1 ) ) ;
+    } else {
+      return( ( analogRead(m_jumpers[0]) > 100 ? 2 : 3) ) ;
+    }
+  #endif
+
+  #ifdef ARDUINO_ARCH_ESP32
+    unsigned char value = 0;
+
+    for (unsigned char i = 0; i < m_nbJumpers; i++) {
+      value <<= 1;
+      if (digitalRead(m_jumpers[i]) == HIGH) value |= 1;
+    }
+    return value+1;
+  #endif
 }
--- LibJumper.h
+++ LibJumper.h
@@ -2,8 +2,8 @@
  * \file 	LibJumper.h
  * \brief 	Le fichier de définition de la classe LibJumper
  * \author  LaboCesson
- * \version 1.0
- * \date    Janvier 2025
+ * \version 2.0
+ * \date    Octobre 2025
  */
 
 #ifndef _LIB_JUMPER_H_
@@ -11,18 +11,13 @@
 
 #include "arduino.h"
 
+#define MAX_NB_JUMPERS 8
+
 typedef enum {
       PAMI_TEAM_A = 0, ///< Jumper non positionné  :..
       PAMI_TEAM_B = 1  ///< Jumper positionne      |..
 } t_pamiTeam;
 
-
-typedef enum {
-      PAMI_SING = 0, ///< Jumper .:: = Chanteur
-      PAMI_NB_1 = 1, ///< Jumper .|: = Groupie 1
-      PAMI_NB_2 = 2, ///< Jumper .:| = Groupie 2
-      PAMI_NB_3 = 3  ///< Jumper .|| = Groupie 3
-} t_pamiNumber;
 
 
 /// \class LibJumper
@@ -31,10 +26,10 @@ class LibJumper
 {
   public:
     LibJumper(
-		unsigned char pinJumperTeam, ///< Le numéro de la pin connectée au Jumper Team
-		unsigned char pinJumperNbH,  ///< Le numéro de la pin connectée au Jumper Numéro de poids 2
-		unsigned char pinJumperNbL   ///< Le numéro de la pin connectée au Jumper Numéro de poids 1
-	);
+		unsigned char  pinJumperTeam, ///< Le numéro de la pin connectée au Jumper Team
+    unsigned char* jumpersPinList, ///< Pointeur vers la liste de pin de type Jumper
+    unsigned char  nbPinInTab     ///< Nombre de pins dans la liste
+    );
 
 	/// \details Retourne le numéro de l'équipe auquel appartient le PAMI
 	/// - \ref PAMI_TEAM_A pour l'équipe A
@@ -42,14 +37,12 @@ class LibJumper
     t_pamiTeam getTeam(void);
 	
 	/// \details Retourne le numéro du PAMI
-	/// - \ref PAMI_SING pour un chanteur
-	/// - \ref PAMI_NB_1 pour la groupie 1
-	/// - \ref PAMI_NB_2 pour la groupie 2
-	/// - \ref PAMI_NB_3 pour la groupie 3
-    t_pamiNumber getPami(void);
+     unsigned char getPami(void);
 
   private:
-    unsigned char m_pinJumperTeam, m_pinJumperNbH, m_pinJumperNbL;
+    unsigned char m_pinJumperTeam;
+    unsigned char m_nbJumpers;
+    unsigned char m_jumpers[MAX_NB_JUMPERS];
 
   protected:
 };

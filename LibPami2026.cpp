08003b94c4547a6d21264225660f102d1e3280df
pascal <14837879+pgravoille@users.noreply.github.com>
2025-11-21 12:13:43 +0100

    Ajout de la librairie LibPami2026

---
+++ LibPami2026.cpp
@@ -0,0 +1,95 @@
+/*!
+ * \file 	LibPami2026.cpp
+ * \brief 	Le fichier source de la classe LibPami2026 
+ * \author  LaboCesson
+ * \version 1.0
+ * \date    Octobre 2026
+ */
+
+#include "LibPami2026.h"
+
+#define PAMI_JUMPER_NB_L  1
+#define PAMI_JUMPER_NB_M  2
+#define PAMI_JUMPER_NB_H  3
+#define PAMI_JUMPER_TEAM  4
+
+#define PAMI_MOTEURS1_IN1 5
+#define PAMI_MOTEURS1_IN2 6
+#define PAMI_MOTEURS1_IN3 7
+#define PAMI_MOTEURS1_IN4 8
+
+#define PAMI_MOTEURS2_IN1 36
+#define PAMI_MOTEURS2_IN2 37
+#define PAMI_MOTEURS2_IN3 38
+#define PAMI_MOTEURS2_IN4 39
+
+#define PAMI_BASE_CHANNEL_MOTEURS1 4
+#define PAMI_BASE_CHANNEL_MOTEURS2 8
+
+#define PAMI_PIN_GPIO_1  9
+#define PAMI_PIN_GPIO_2 10
+#define PAMI_PIN_GPIO_3 11
+#define PAMI_PIN_GPIO_4 12
+#define PAMI_PIN_GPIO_5 13
+#define PAMI_PIN_GPIO_6 14
+
+#define PAMI_AFF1637_DATA 15
+#define PAMI_AFF1637_CLK  16
+
+#define PAMI_433MHZ_PIN         40
+#define PAMI_433MHZ_SPEED     2000
+#define PAMI_433MHZ_PATTERN "LaboCesson"
+
+
+unsigned char gpioPinListBasic[] = {PAMI_PIN_GPIO_1,PAMI_PIN_GPIO_2,PAMI_PIN_GPIO_3,PAMI_PIN_GPIO_4};
+unsigned char nbGpioPinBasic = 4;
+
+unsigned char gpioPinListNinja[] = {PAMI_PIN_GPIO_1,PAMI_PIN_GPIO_2,PAMI_PIN_GPIO_3,PAMI_PIN_GPIO_4,PAMI_PIN_GPIO_5,PAMI_PIN_GPIO_6};
+unsigned char nbGpioPinNinja = 6;
+
+unsigned char jumpersPinList[] = {PAMI_JUMPER_NB_H,PAMI_JUMPER_NB_M,PAMI_JUMPER_NB_L};
+
+
+LibPami2026Basic::LibPami2026Basic() :
+  afficheur(PAMI_AFF1637_CLK,PAMI_AFF1637_DATA),
+  jumper(PAMI_JUMPER_TEAM, jumpersPinList, 3),
+  moteur(PAMI_PIN_GPIO_5, PAMI_PIN_GPIO_6),
+  gpio(gpioPinListBasic,nbGpioPinBasic),
+  gyro(),
+  radio(RADIO433MHZ_RECV, PAMI_433MHZ_SPEED, PAMI_433MHZ_PIN, PAMI_433MHZ_PATTERN),
+  chrono()
+{
+  // LibChrono::setDisplay(dynamic_cast<LibAff1637*>(this));
+  gyro.setDisplay(&afficheur);
+  chrono.setDisplay(&afficheur);
+}
+
+
+void LibPami2026Basic::gestion(void) {
+  afficheur.gestion();
+  gyro.gestion();
+  chrono.gestion();
+}
+
+
+LibPami2026Ninja::LibPami2026Ninja() :
+  afficheur(PAMI_AFF1637_CLK,PAMI_AFF1637_DATA),
+  jumper(PAMI_JUMPER_TEAM, jumpersPinList, 3),
+  moteur1(PAMI_MOTEURS1_IN1, PAMI_MOTEURS1_IN2, PAMI_MOTEURS1_IN3, PAMI_MOTEURS1_IN4,PAMI_BASE_CHANNEL_MOTEURS1),
+  moteur2(PAMI_MOTEURS2_IN1, PAMI_MOTEURS2_IN2, PAMI_MOTEURS2_IN3, PAMI_MOTEURS2_IN4,PAMI_BASE_CHANNEL_MOTEURS2),
+  gpio(gpioPinListNinja,nbGpioPinNinja),
+  gyro(),
+  radio(RADIO433MHZ_RECV, PAMI_433MHZ_SPEED, PAMI_433MHZ_PIN, PAMI_433MHZ_PATTERN),
+  chrono()
+{
+  gyro.setDisplay(&afficheur);
+  chrono.setDisplay(&afficheur);
+}
+
+
+void LibPami2026Ninja::gestion(void) {
+  afficheur.gestion();
+  gyro.gestion();
+  chrono.gestion();
+}
+
---
+++ LibPami2026.h
@@ -0,0 +1,87 @@
+/*!
+ * \file 	LibPami2026.h
+ * \brief 	Le fichier de définition de la classe LibPami2026
+ * \author  LaboCesson
+ * \version 1.0
+ * \date    Octobre 2026
+ */
+
+
+
+#ifndef _LIB_PAMI2026_H_
+#define _LIB_PAMI2026_H_
+
+#include "arduino.h"
+
+#include "LibAff1637.h"
+#include "LibJumper.h"
+#include "LibMoteur.h"
+#include "LibGpio.h"
+// #include "LibUltrason.h"
+#include "LibGyroscope.h"
+#include "Lib433Mhz.h"
+#include "LibChrono.h"
+
+typedef enum {
+  PAMI_GPIO_1 = 0, ///< GPIO1
+  PAMI_GPIO_2 = 1, ///< GPIO2
+  PAMI_GPIO_3 = 2, ///< GPIO3
+  PAMI_GPIO_4 = 3, ///< GPIO4
+  PAMI_GPIO_5 = 4, ///< GPIO5
+  PAMI_GPIO_6 = 5  ///< GPIO6
+} t_pami_gpio;
+
+
+/// \class LibPami2025
+/// \brief Librairies de gestion des PAMI de l'année 2026
+/// \details Librairies requises :
+/// \details    MPU6050   : https://github.com/electroniccats/mpu6050
+/// \details    Servo     : https://docs.arduino.cc/libraries/servo/
+/// \details    RadioHead : https://www.airspayce.com/mikem/arduino/RadioHead
+/// \details   Wire
+
+class LibPami2026Basic
+{
+  public:
+    LibPami2026Basic();
+
+    /// \details Cette fonction doit être appelée régulièrement pour la gestion des différents éléments d'un PAMI
+    void gestion(void);
+
+    LibAff1637   afficheur; ///< Librairie de gestion d'un afficheur
+    LibJumper    jumper;    ///< Librairie de gestion des jumpers d'un PAMI
+    LibGpio      gpio;      ///< Librairie de gestion des GPIO
+    LibGyroscope gyro;      ///< Librairie de gestion d'un gyroscope
+    LibMoteur    moteur;    ///< Librairie de gestion d'un couple de moteur
+    Lib433Mhz    radio;     ///< Librairie de gestion d'un récepteur 433Mhz
+    LibChrono    chrono;    ///< Librairie de gestion d'un chronometre
+
+  private:
+
+  protected:
+};
+
+class LibPami2026Ninja
+{
+  public:
+    LibPami2026Ninja();
+
+    /// \details Cette fonction doit être appelée régulièrement pour la gestion des différents éléments d'un PAMI
+    void gestion(void);
+
+    LibAff1637   afficheur; ///< Librairie de gestion d'un afficheur
+    LibJumper    jumper;    ///< Librairie de gestion des jumpers d'un PAMI
+    LibGpio      gpio;      ///< Librairie de gestion des GPIO
+    LibGyroscope gyro;      ///< Librairie de gestion d'un gyroscope
+    LibMoteur    moteur1;   ///< Librairie de gestion d'un couple de moteur
+    LibMoteur    moteur2;   ///< Librairie de gestion d'un couple de moteur (cas omnidirectionnel)
+    Lib433Mhz    radio;     ///< Librairie de gestion d'un récepteur 433Mhz
+    LibChrono    chrono;    ///< Librairie de gestion d'un chronometre
+
+  private:
+
+  protected:
+};
+
+
+#endif
